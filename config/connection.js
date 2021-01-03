4274e8334c2516c1c9250bc568e8ec929b66c463
ard847 <renatopombal7@gmail.com>
2021-01-03 13:30:47 +0000

    s

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql');
 require("dotenv").config()
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.dbPassword,
-    database: 'burger',
-  });
+console.log(process.env.connection)
+const connection = mysql.createConnection(process.env.connection);
  
 module.exports = connection
